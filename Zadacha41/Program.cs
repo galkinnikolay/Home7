1d3197b8e9772ab14093eafbfabbe903ab69e66f
galkinnikolay <galkin_nikolay@icloud.com>
2023-04-27 15:13:49 +0300

    .\Zadacha41\

--- Zadacha41/Program.cs
+++ Zadacha41/Program.cs
@@ -9,7 +9,7 @@
 double[] array = new double[5];
 for (int i = 0; i < array.Length; i++)
 {
-    double number = new Random().Next(-1000, 1001) + new Random().NextDouble(); // 8 + 0.22 = 8.22
+    double number = new Random().Next(-10, 10) + new Random().NextDouble(); // 8 + 0.22 = 8.22
     array[i] =  Math.Round(number, 2); 
 }
 for (int i = 0; i < array.Length; i++)
@@ -29,3 +29,35 @@ for (int i = 0; i < array.Length; i++)
 double result = count;
 Console.WriteLine(" ");
 Console.WriteLine($"Количество положительных элементов: {result}");
+
+
+
+// Console.Write("Введите количество элементов массива: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// int[] Array = new int[m];
+
+// void mas(int m)
+// {
+// for (int i = 0; i < m; i++)
+// {
+// Console.WriteLine($"Введите {i+1} элемент массива ");
+// Array[i] = Convert.ToInt32(Console.ReadLine());
+// }
+
+// }
+
+// int kol(int[] Array)
+// {
+// int i=0;
+// int sum = 0;
+// while (i < Array.Length)
+// {
+// if(Array[i]>0)
+// sum = sum + 1;
+// i = i + 1;
+// }
+// return sum;
+// }
+
+// mas(m);
+// Console.Write($"n Чисел больше нуля: {kol(Array)}");
